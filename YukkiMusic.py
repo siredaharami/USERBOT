8c75dff3dabe6755db2a4355140ccd676e8dd4fa
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2024-12-05 12:35:12 +0530

    Update YukkiMusic.py

--- YukkiMusic.py
+++ YukkiMusic.py
@@ -1,7 +1,6 @@
 import aiohttp, aiofiles, asyncio, base64, logging
 import os, platform, random, re, socket
 import sys, time, textwrap
-import SUDO_USERS
 
 from os import getenv
 from io import BytesIO
