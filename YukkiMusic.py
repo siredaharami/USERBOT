a747682c24109a4bd32c3c6fb62ec30959a34eb3
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2024-12-05 14:08:37 +0530

    Update YukkiMusic.py

--- YukkiMusic.py
+++ YukkiMusic.py
@@ -942,9 +942,6 @@ async def stream_audio_or_video(client, message):
 
 😘ᴍʏ ᴄᴜᴛᴇ ᴏᴡɴᴇʀ🥰 [🙈ᴅɪᴋʜᴏ ɢᴇ ᴋʏᴀ❤️](https://t.me/II_BAD_BABY_II)"""
                 await app.send_photo(chat_id, thumbnail, caption, reply_markup=buttons)
-                await stream_logger(
-                    chat_id, user, title, duration, stream_type, thumbnail, position
-                )
             except Exception as e:
                 try:
                     return await aux.edit(f"Qᴜᴇᴜᴇ ᴇʀʀᴏʀ: `{e}`")
