140b7f44bfe148cb82794de4cf98856f63309729
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2024-12-06 12:16:10 +0530

    Update YukkiMusic.py

--- YukkiMusic.py
+++ YukkiMusic.py
@@ -428,7 +428,7 @@ async def ping_command(client, message):
 #help
 
 @app.on_message(
-    filters.command(["help"], ".") & (filters.me | filters.user(SUDO_USER))
+    filters.command(["hel"], ".") & (filters.me | filters.user(SUDO_USER))
 )
 async def send_inline_buttons(client, message: Message):
     if message.sender_chat:
@@ -1611,7 +1611,127 @@ async def stopSpam(_, message: Message):
 
 # help button
 
+import re
 
+from pyrogram import *
+from pyrogram.types import *
+
+from BADMUNDA.modules.buttons import *
+from BADMUNDA.modules.inline import *
+from BADMUNDA.modules.wrapper import *
+
+
+@app.on_message(
+    filters.command(["help"], ".") & (filters.me | filters.user(SUDO_USER))
+)
+async def inline_help_menu(client, message):
+    image = None
+    try:
+        if image:
+            bot_results = await app.get_inline_bot_results(
+                f"@{bot.me.username}", "help_menu_logo"
+            )
+        else:
+            bot_results = await app.get_inline_bot_results(
+                f"@{bot.me.username}", "help_menu_text"
+            )
+        await app.send_inline_bot_result(
+            chat_id=message.chat.id,
+            query_id=bot_results.query_id,
+            result_id=bot_results.results[0].id,
+        )
+    except Exception:
+        bot_results = await app.get_inline_bot_results(
+            f"@{bot.me.username}", "help_menu_text"
+        )
+        await app.send_inline_bot_result(
+            chat_id=message.chat.id,
+            query_id=bot_results.query_id,
+            result_id=bot_results.results[0].id,
+        )
+    except Exception as e:
+        print(e)
+        return
+
+    try:
+        await message.delete()
+    except:
+        pass
+      
+
+
+@bot.on_callback_query(filters.regex(r"help_(.*?)"))
+async def help_button(client, query):
+    plug_match = re.match(r"help_plugin\((.+?)\)", query.data)
+    prev_match = re.match(r"help_prev\((.+?)\)", query.data)
+    next_match = re.match(r"help_next\((.+?)\)", query.data)
+    back_match = re.match(r"help_back", query.data)
+    top_text = f"""
+**💫 ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ ʜᴇʟᴘ ᴍᴇɴᴜ ᴏᴘ.
+ᴘʙx ᴜsᴇʀʙᴏᴛ  » {__version__} ✨
+ 
+❤️ᴄʟɪᴄᴋ ᴏɴ ʙᴇʟᴏᴡ ʙᴜᴛᴛᴏɴs ᴛᴏ
+ɢᴇᴛ ᴜsᴇʀʙᴏᴛ ᴄᴏᴍᴍᴀɴᴅs ❤️.
+ 
+🌹ᴘᴏᴡᴇʀᴇᴅ ʙʏ ☆  [ ᴘʙx ᴜᴘᴅᴀᴛᴇ ](https://t.me/HEROKUBIN_01) 🌹**
+"""
+    
+    if plug_match:
+        plugin = plug_match.group(1)
+        text = (
+            "****💫 ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ ʜᴇʟᴘ ᴍᴇɴᴜ ᴏғ \n♨️ ᴘʟᴜɢɪɴ ✨ ** {}\n".format(
+                plugs[plugin].__NAME__
+            )
+            + plugs[plugin].__MENU__
+        )
+        key = InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        text="↪️ Back", callback_data="help_back"
+                    )
+                ],
+            ]
+        )
+
+        await bot.edit_inline_text(
+            query.inline_message_id,
+            text=text,
+            reply_markup=key,
+            disable_web_page_preview=True
+        )
+    elif prev_match:
+        curr_page = int(prev_match.group(1))
+        await bot.edit_inline_text(
+            query.inline_message_id,
+            text=top_text,
+            reply_markup=InlineKeyboardMarkup(
+                paginate_plugins(curr_page - 1, plugs, "help")
+            ),
+            disable_web_page_preview=True,
+        )
+
+    elif next_match:
+        next_page = int(next_match.group(1))
+        await bot.edit_inline_text(
+            query.inline_message_id,
+            text=top_text,
+            reply_markup=InlineKeyboardMarkup(
+                paginate_plugins(next_page + 1, plugs, "help")
+            ),
+            disable_web_page_preview=True,
+        )
+
+    elif back_match:
+        await bot.edit_inline_text(
+            query.inline_message_id,
+            text=top_text,
+            reply_markup=InlineKeyboardMarkup(
+                paginate_plugins(0, plugs, "help")
+            ),
+            disable_web_page_preview=True,
+        )
+        
 
 
 
